2171eb3965e545cea92a3c1764ca5784cdf3bb4e
Liam Reardon <lmr647@mun.ca>
2019-01-11 14:48:11 -0330

    init website

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Liam Reardon';
+  linkedURL = "https://www.linkedin.com/in/liam-reardon-0b74a9159";
+  gitURL = "https://github.com/liamreardon";
+  twitterURL = "https://twitter.com/lreardon22"
+}
