f33e71b547ae640d8ddf9cb82b8fdcdd81e31739
Liam Reardon <lmr647@mun.ca>
2019-01-17 02:01:14 -0330

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { EducationComponent } from './components/education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule
