4c6390ce0f2b9e5b54ba6c927cec38fed92e6b17
Liam Reardon <lmr647@mun.ca>
2019-02-18 02:12:44 -0330

    add sidebar

--- src/app/models/static/my-projects.ts
+++ src/app/models/static/my-projects.ts
@@ -6,5 +6,11 @@ export const PROJECTS: Project[] = [
         description: 'Picture Messaging iOS App',
         img: 'assets/img/phantom.png',
         link: 'https://github.com/liamreardon/Phantom-Chat'
+    },
+    {
+        name: 'CS 2005 Collaborative Tool',
+        description: 'Collaborative Tool for students in CS 2005 Software Engineering',
+        img: 'assets/img/2005.png',
+        link: 'https://github.com/liamreardon/CS-2005-Collaborative-Tool'
     }
 ]
