04882f96a974d1a4bee584a06dcca50683a34c35
Liam Reardon <lmr647@mun.ca>
2019-02-23 01:35:19 -0330

    mod sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -10,7 +10,8 @@ export class SidebarComponent implements OnInit {
   linkedURL = "https://www.linkedin.com/in/liam-reardon-0b74a9159";
   gitURL = "https://github.com/liamreardon";
   twitterURL = "https://twitter.com/lreardon22";
-  email = "mailto:me@liamreardon.io"
+  email = "mailto:me@liamreardon.io";
+  spotify = "https://open.spotify.com/user/12185352237?si=03vv6ZlHTUGiofZRbddlTw";
 
   constructor() { }
 
