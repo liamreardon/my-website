091ea4a8dcdb67a36e86771d6ca006289de8ead0
Liam Reardon <lmr647@mun.ca>
2019-05-20 14:46:50 -0230

    side menu styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,28 +16,16 @@ declare const window: any;
 })
 export class AppComponent implements OnInit {
 
-  @HostListener("window:scroll", [])
-  onWindowScroll() {
-
-    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    console.log(number)
-    
-    if (number > 50) {
-      this.navColor = 'white'
-      this.logo = "assets/img/LR-red.png"
-      this.navTextColor = '#d91e18'
-    } else if (number < 50) {
-      this.navColor = 'rgba(46, 49, 49, 0)'
-      this.logo = "assets/img/LR.png"
-      this.navTextColor = 'white'
-    }
-
-  }
   
   isCollapsed: boolean = true
   navColor = 'rgba(46, 49, 49, 0)'
   navTextColor = 'white'
   logo = "assets/img/LR.png"
+  linkedURL = "https://www.linkedin.com/in/liam-reardon-0b74a9159";
+  gitURL = "https://github.com/liamreardon";
+  twitterURL = "https://twitter.com/lreardon22";
+  email = "mailto:me@liamreardon.io";
+  spotify = "https://open.spotify.com/user/12185352237?si=03vv6ZlHTUGiofZRbddlTw";
 
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
@@ -46,6 +34,23 @@ export class AppComponent implements OnInit {
   );
 
   constructor(private router: Router, private breakpointObserver: BreakpointObserver) { }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+
+    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    
+    if (number > 50) {
+      this.navColor = 'white'
+      this.logo = "assets/img/LR-red.png"
+      this.navTextColor = '#151515'
+    } else if (number < 50) {
+      this.navColor = 'rgba(46, 49, 49, 0)'
+      this.logo = "assets/img/LR.png"
+      this.navTextColor = 'white'
+    }
+
+  }
 
   ngOnInit() {
     this.router.events.subscribe((evt) => {
